4ed3cd95e05067402abcccd4ecb8e099756392ae
Aragaw Mebratu <aragawmebratu65@gmail.com>
2026-03-07 14:50:07 +0300

    final

--- singlesession/db/events.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$observers = [
-    [
-        'eventname' => '\core\event\user_loggedin',
-        'callback'  => '\local_singlesession\observer::user_loggedin',
-    ],
-];
